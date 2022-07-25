06e4b788920421b82293d2237022f8f3ab740b3b
Zhao KK <kikakkz@hotmail.com>
2022-07-25 15:48:08 +0000

    Use unsold for unsold

--- pkg/client/profit/unsold/unsold.go
+++ pkg/client/profit/unsold/unsold.go
@@ -1,5 +1,5 @@
 //nolint:dupl
-package detail
+package unsold
 
 import (
 	"context"
@@ -9,14 +9,14 @@ import (
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
 
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
-	npool "github.com/NpoolPlatform/message/npool/miningmgr/profit/detail"
+	npool "github.com/NpoolPlatform/message/npool/miningmgr/profit/unsold"
 
 	constant "github.com/NpoolPlatform/mining-manager/pkg/message/const"
 )
 
 var timeout = 10 * time.Second
 
-type handler func(context.Context, npool.ProfitDetailClient) (cruder.Any, error)
+type handler func(context.Context, npool.ProfitUnsoldClient) (cruder.Any, error)
 
 func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
 	_ctx, cancel := context.WithTimeout(ctx, timeout)
@@ -24,144 +24,144 @@ func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
 
 	conn, err := grpc2.GetGRPCConn(constant.ServiceName, grpc2.GRPCTAG)
 	if err != nil {
-		return nil, fmt.Errorf("fail get detail connection: %v", err)
+		return nil, fmt.Errorf("fail get unsold connection: %v", err)
 	}
 
 	defer conn.Close()
 
-	cli := npool.NewProfitDetailClient(conn)
+	cli := npool.NewProfitUnsoldClient(conn)
 
 	return handler(_ctx, cli)
 }
 
-func CreateDetail(ctx context.Context, in *npool.DetailReq) (*npool.Detail, error) {
-	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitDetailClient) (cruder.Any, error) {
-		resp, err := cli.CreateDetail(ctx, &npool.CreateDetailRequest{
+func CreateUnsold(ctx context.Context, in *npool.UnsoldReq) (*npool.Unsold, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitUnsoldClient) (cruder.Any, error) {
+		resp, err := cli.CreateUnsold(ctx, &npool.CreateUnsoldRequest{
 			Info: in,
 		})
 		if err != nil {
-			return nil, fmt.Errorf("fail create detail: %v", err)
+			return nil, fmt.Errorf("fail create unsold: %v", err)
 		}
 		return resp.GetInfo(), nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("fail create detail: %v", err)
+		return nil, fmt.Errorf("fail create unsold: %v", err)
 	}
-	return info.(*npool.Detail), nil
+	return info.(*npool.Unsold), nil
 }
 
-func CreateDetails(ctx context.Context, in []*npool.DetailReq) ([]*npool.Detail, error) {
-	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitDetailClient) (cruder.Any, error) {
-		resp, err := cli.CreateDetails(ctx, &npool.CreateDetailsRequest{
+func CreateUnsolds(ctx context.Context, in []*npool.UnsoldReq) ([]*npool.Unsold, error) {
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitUnsoldClient) (cruder.Any, error) {
+		resp, err := cli.CreateUnsolds(ctx, &npool.CreateUnsoldsRequest{
 			Infos: in,
 		})
 		if err != nil {
-			return nil, fmt.Errorf("fail create details: %v", err)
+			return nil, fmt.Errorf("fail create unsolds: %v", err)
 		}
 		return resp.GetInfos(), nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("fail create details: %v", err)
+		return nil, fmt.Errorf("fail create unsolds: %v", err)
 	}
-	return infos.([]*npool.Detail), nil
+	return infos.([]*npool.Unsold), nil
 }
 
-func GetDetail(ctx context.Context, id string) (*npool.Detail, error) {
-	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitDetailClient) (cruder.Any, error) {
-		resp, err := cli.GetDetail(ctx, &npool.GetDetailRequest{
+func GetUnsold(ctx context.Context, id string) (*npool.Unsold, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitUnsoldClient) (cruder.Any, error) {
+		resp, err := cli.GetUnsold(ctx, &npool.GetUnsoldRequest{
 			ID: id,
 		})
 		if err != nil {
-			return nil, fmt.Errorf("fail get detail: %v", err)
+			return nil, fmt.Errorf("fail get unsold: %v", err)
 		}
 		return resp.GetInfo(), nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("fail get detail: %v", err)
+		return nil, fmt.Errorf("fail get unsold: %v", err)
 	}
-	return info.(*npool.Detail), nil
+	return info.(*npool.Unsold), nil
 }
 
-func GetDetailOnly(ctx context.Context, conds *npool.Conds) (*npool.Detail, error) {
-	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitDetailClient) (cruder.Any, error) {
-		resp, err := cli.GetDetailOnly(ctx, &npool.GetDetailOnlyRequest{
+func GetUnsoldOnly(ctx context.Context, conds *npool.Conds) (*npool.Unsold, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitUnsoldClient) (cruder.Any, error) {
+		resp, err := cli.GetUnsoldOnly(ctx, &npool.GetUnsoldOnlyRequest{
 			Conds: conds,
 		})
 		if err != nil {
-			return nil, fmt.Errorf("fail get detail: %v", err)
+			return nil, fmt.Errorf("fail get unsold: %v", err)
 		}
 		return resp.GetInfo(), nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("fail get detail: %v", err)
+		return nil, fmt.Errorf("fail get unsold: %v", err)
 	}
-	return info.(*npool.Detail), nil
+	return info.(*npool.Unsold), nil
 }
 
-func GetDetails(ctx context.Context, conds *npool.Conds, limit, offset int32) ([]*npool.Detail, uint32, error) {
+func GetUnsolds(ctx context.Context, conds *npool.Conds, limit, offset int32) ([]*npool.Unsold, uint32, error) {
 	var total uint32
-	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitDetailClient) (cruder.Any, error) {
-		resp, err := cli.GetDetails(ctx, &npool.GetDetailsRequest{
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitUnsoldClient) (cruder.Any, error) {
+		resp, err := cli.GetUnsolds(ctx, &npool.GetUnsoldsRequest{
 			Conds:  conds,
 			Limit:  limit,
 			Offset: offset,
 		})
 		if err != nil {
-			return nil, fmt.Errorf("fail get details: %v", err)
+			return nil, fmt.Errorf("fail get unsolds: %v", err)
 		}
 		total = resp.GetTotal()
 		return resp.GetInfos(), nil
 	})
 	if err != nil {
-		return nil, 0, fmt.Errorf("fail get details: %v", err)
+		return nil, 0, fmt.Errorf("fail get unsolds: %v", err)
 	}
-	return infos.([]*npool.Detail), total, nil
+	return infos.([]*npool.Unsold), total, nil
 }
 
-func ExistDetail(ctx context.Context, id string) (bool, error) {
-	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitDetailClient) (cruder.Any, error) {
-		resp, err := cli.ExistDetail(ctx, &npool.ExistDetailRequest{
+func ExistUnsold(ctx context.Context, id string) (bool, error) {
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitUnsoldClient) (cruder.Any, error) {
+		resp, err := cli.ExistUnsold(ctx, &npool.ExistUnsoldRequest{
 			ID: id,
 		})
 		if err != nil {
-			return nil, fmt.Errorf("fail get detail: %v", err)
+			return nil, fmt.Errorf("fail get unsold: %v", err)
 		}
 		return resp.GetInfo(), nil
 	})
 	if err != nil {
-		return false, fmt.Errorf("fail get detail: %v", err)
+		return false, fmt.Errorf("fail get unsold: %v", err)
 	}
 	return infos.(bool), nil
 }
 
-func ExistDetailConds(ctx context.Context, conds *npool.Conds) (bool, error) {
-	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitDetailClient) (cruder.Any, error) {
-		resp, err := cli.ExistDetailConds(ctx, &npool.ExistDetailCondsRequest{
+func ExistUnsoldConds(ctx context.Context, conds *npool.Conds) (bool, error) {
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitUnsoldClient) (cruder.Any, error) {
+		resp, err := cli.ExistUnsoldConds(ctx, &npool.ExistUnsoldCondsRequest{
 			Conds: conds,
 		})
 		if err != nil {
-			return nil, fmt.Errorf("fail get detail: %v", err)
+			return nil, fmt.Errorf("fail get unsold: %v", err)
 		}
 		return resp.GetInfo(), nil
 	})
 	if err != nil {
-		return false, fmt.Errorf("fail get detail: %v", err)
+		return false, fmt.Errorf("fail get unsold: %v", err)
 	}
 	return infos.(bool), nil
 }
 
-func CountDetails(ctx context.Context, conds *npool.Conds) (uint32, error) {
-	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitDetailClient) (cruder.Any, error) {
-		resp, err := cli.CountDetails(ctx, &npool.CountDetailsRequest{
+func CountUnsolds(ctx context.Context, conds *npool.Conds) (uint32, error) {
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ProfitUnsoldClient) (cruder.Any, error) {
+		resp, err := cli.CountUnsolds(ctx, &npool.CountUnsoldsRequest{
 			Conds: conds,
 		})
 		if err != nil {
-			return nil, fmt.Errorf("fail count detail: %v", err)
+			return nil, fmt.Errorf("fail count unsold: %v", err)
 		}
 		return resp.GetInfo(), nil
 	})
 	if err != nil {
-		return 0, fmt.Errorf("fail count detail: %v", err)
+		return 0, fmt.Errorf("fail count unsold: %v", err)
 	}
 	return infos.(uint32), nil
 }
